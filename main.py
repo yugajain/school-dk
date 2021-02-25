84db9f49aa93c923be82c1e15e13246f8ce6a295
yuga jain <64032784+yugajain@users.noreply.github.com>
2021-02-25 10:18:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print("this is a program for ")
-side_1 = int(input("write the first side: "))
-side_2 = int(input("write the second side: "))
-side_3 = int(input("write the third side: "))
-side_4 = int(input("write the fourth side: "))
-print(side_1 + side_2 + side_3 + side_4 , "is the perimeter")
-
-
+r = int(input("input: "))
+if r==0:
+    print("area = 0")
+else:
+    print("area =", 22/7*r*r)
